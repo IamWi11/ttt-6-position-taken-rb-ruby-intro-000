cfb51aea8acd6ee6875806fc54a26fdbe0393658
William <william.l.munoz@gmail.com>
2020-04-14 00:21:59 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,9 @@
 def position_taken?(board, index)
 
-array[1] == " "
-
+if (board[index] == " " || board[index] == "" || board[index] == nil)
+  return false
+else 
+  return true 
+  end
 end
-
 # code your #position_taken? method here!
